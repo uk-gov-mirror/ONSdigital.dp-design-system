c4d54953aeda42b6b35dc68abab05b604e80d614
Daniel W <danieldwalford@gmail.com>
2021-03-08 15:46:06 +0000

    Added linter to pipeline and ran a fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
-const autoprefixer = require('gulp-autoprefixer');
+const autoprefixer = require('autoprefixer');
 const postcss = require("gulp-postcss")
-const cssnano = require("gulp-cssnano")
+const cssnano = require("cssnano")
+const bemlinter = require("postcss-bem-linter")
+const reporter = require("postcss-reporter");
+const sasslint = require("sass-lint")
+const stylelint = require("gulp-stylelint")
 
 sass.compiler = require('dart-sass');
 
@@ -17,14 +21,17 @@ const sassOptionsProd = {
     errLogToConsole: true,
     outputStyle: 'compressed'
 };
+const plugins = [autoprefixer({grid: "autoplace"}), cssnano(), bemlinter({preset: 'bem'}), reporter()]
+const reporters = [{formatter: "verbose", console: true}]
 
 // Will generate source maps, transpile SCSS into CSS, prefix styles with vendor prefixes then optimise the final css size
 gulp.task('css', function (done) {
     return gulp
         .src(input)
         .pipe(sourcemaps.init())
+        .pipe(stylelint({reporters}))
         .pipe(sass(sassOptionsDev).on('error', sass.logError))
-        .pipe(postcss([autoprefixer, cssnano]))
+        .pipe(postcss(plugins))
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(output))
         .on('end', done);
@@ -47,6 +54,6 @@ gulp.task('build', gulp.series('css', function () {
     return gulp
         .src(input)
         .pipe(sass(sassOptionsProd).on('error', sass.logError))
-        .pipe(postcss([autoprefixer, cssnano]))
+        .pipe(postcss(plugins))
         .pipe(gulp.dest(output));
 }));
